c08da2cc2ed8e77125a7806f8e81171016038b67
ludmi2il <ludmidosil@gmail.com>
2024-08-27 12:21:52 -0300

    Primera version de Usuario.java

---
+++ src/Usuario.java
@@ -0,0 +1,17 @@
+public class Usuario {
+    private String nombres;
+    private String apellidos;
+    private String correo;
+    private String contrasena;
+    private String telefono;
+    private String fechaNacimiento;
+    private String pais;
+    private byte metodoAutenticacion;
+
+
+
+
+
+    public void elegirMedioDeAutenticacion() {
+    }
+}
