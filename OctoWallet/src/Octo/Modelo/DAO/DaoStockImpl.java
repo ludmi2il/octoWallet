d0e3c9352d3644b3358335ccc77c9b8bd441196b
Giovanna Gotusso Farina <140980560+GioGotusso@users.noreply.github.com>
2024-10-21 16:02:27 -0300

    Add files via upload
    version modificada de DaoStockImpl con crear,listar y obtener implementados

--- OctoWallet/src/Octo/Modelo/DAO/DaoStockImpl.java
+++ OctoWallet/src/Octo/Modelo/DAO/DaoStockImpl.java
@@ -2,21 +2,59 @@ package Octo.Modelo.DAO;
 
 import Octo.Modelo.Entidad.Stock;
 
+import java.sql.SQLException;
 import java.util.List;
 
 public class DaoStockImpl implements  DaoStock{
     @Override
     public void crear(Stock dato) {
+        try {
+            Statement st = Conexion.getConexion().createStatement();
+            String sql = "INSERT INTO STOCK (CANTIDAD,NOMENCLATURA)" +
+                         "VALUES('" + dato.getCantidad() + "', '"+ dato.getNomenclatura() + "', ';";
+            // se puede usar sets de Statement y los campos para evitar errores de tipeo.
+             st.executeUpdate(sql);
+             st.close();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        }
 
     }
 
     @Override
     public List<Stock> listar() {
-        return null;
+        List <Stock> stocks = new ArrayList<>();
+        try {
+            Statement st = Conexion.getConexion().createStatement();
+            ResultSet res = st.executeQuery("SELECT * FROM STOCK");
+            while (res.next()) {
+                stocks.add(convertir(res));
+            }
+            res.close();
+            st.close();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        }
     }
 
     @Override
-    public Stock obtener(String id) {
-        return null;
+    public Stock obtener(String nomenclatura) {
+        Stock stock = null;
+        try {
+            String str = "SELECT * FROM STOCK WHERE NOMENCLATURA = ?";
+            PreparedStatement st = Conexion.getConexion().prepareStatement(str);
+            st.setString(1,nomenclatura);
+            ResultSet res = st.executeQuery();
+            if (res.next()) {
+                stock = convertir(res);
+            }
+            res.close();
+            st.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return stock;
     }
 }
