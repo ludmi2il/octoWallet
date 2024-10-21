88ad1217ab9f925bb4b5b9e9d13f36592d07deab
Giovanna Gotusso Farina <140980560+GioGotusso@users.noreply.github.com>
2024-10-21 16:05:19 -0300

    Add files via upload
    version modificada del controladorAIO con la implementacion crear cantidad necesaria para stock

--- OctoWallet/src/Octo/Controlador/ControladorAIO.java
+++ OctoWallet/src/Octo/Controlador/ControladorAIO.java
@@ -4,4 +4,10 @@ public class ControladorAIO {
     /* controlador all in one
      será la interfaz de programador desde la cual main se comunicará y creará los objetos necesarios, y llamará
     a los objetos pertinentes*/
+
+    public int crearCantidad (){ //falta ver realmente donde va a ir esto
+        int cant;
+        cant = random.nextInt(10000) + 1;
+        return cant;
+    }
 }
