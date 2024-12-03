6d43da7c064286608f77d68e82bda53147e73ca2
francisco <franciscoestrada565@gmail.com>
2024-12-03 17:00:39 -0300

    Entregable3 - BD actualizada, hay que cambiar los modelos

--- OctoWallet/src/Octo/Modelo/JDBC/Conexion.java
+++ OctoWallet/src/Octo/Modelo/JDBC/Conexion.java
@@ -15,8 +15,7 @@ public class Conexion {
             conexion = DriverManager.getConnection(URL);
             if (conexion != null) {
                 creacionDeTablasEnBD();
-                System.out.println("Conexión establecida con la base de datos.");
-            }
+                System.out.println("Conexión establecida con la base de datos.");}
         } catch( SQLException e) {
             System.out.println(e.getMessage());
             System.out.println("ERROR AL HACER CONEXION CON LA BBDD");
@@ -42,39 +41,70 @@ public class Conexion {
         }
 
     }
-    public static void creacionDeTablasEnBD() throws
+    /**
+     * Este método se encarga de la creación de las tablas.
+     *
+     * @throws SQLException
+     */
+    private static void creacionDeTablasEnBD() throws
             SQLException {
         Statement stmt;
         stmt = conexion.createStatement();
-        String sql = "CREATE TABLE IF NOT EXISTS MONEDA "
+        String sql = "CREATE TABLE IF NOT EXISTS PERSONA "
                 + "("
-                + " TIPO       VARCHAR(1)    NOT NULL, "
-                + " NOMBRE       VARCHAR(50)    NOT NULL, "
-                + " NOMENCLATURA VARCHAR(10)  PRIMARY KEY   NOT NULL, "
-                + " VALOR_DOLAR	REAL     NOT NULL, "
-                + " VOLATILIDAD	REAL     NULL, "
-                + " STOCK	REAL     NULL "  + ")";
+                + " ID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL , "
+                + " NOMBRES VARCHAR(50) NOT NULL, "
+                + " APELLIDOS VARCHAR(50) NOT NULL "
+                + ")";
         stmt.executeUpdate(sql);
-        sql = "CREATE TABLE IF NOT EXISTS STOCK "
-                + "( "
-                + " NOMENCLATURA VARCHAR(10) PRIMARY KEY NOT NULL, "
-                + " CANTIDAD REAL NOT NULL "
+        sql = "CREATE TABLE IF NOT EXISTS USUARIO " + "(" +
+                " ID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL , "
+                + " ID_PERSONA INTEGER NOT NULL, "
+                + " EMAIL VARCHAR(50) NOT NULL, "
+                + " PASSWORD VARCHAR(50) NOT NULL, "
+                + " ACEPTA_TERMINOS BOOLEAN NOT NULL, "
+                + " FOREIGN KEY(ID_PERSONA) REFERENCES PERSONA(ID)"
+                + ")";
+        stmt.executeUpdate(sql);
+        sql = "CREATE TABLE IF NOT EXISTS MONEDA "
+                + "("
+                + " ID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL , " +
+                " TIPO VARCHAR(1) NOT NULL, "
+                + " NOMBRE VARCHAR(50) NOT NULL, "
+                + " NOMENCLATURA VARCHAR(10) NOT NULL, "
+                + " VALOR_DOLAR REAL NOT NULL, "
+                + " VOLATILIDAD REAL NULL, "
+                + " STOCK REAL NULL, "
+                + " NOMBRE_ICONO VARCHAR(50) NOT NULL "
                 + ")";
         stmt.executeUpdate(sql);
         sql = "CREATE TABLE IF NOT EXISTS ACTIVO_CRIPTO"
                 + "("
-                + " NOMENCLATURA VARCHAR(10)  PRIMARY KEY     NOT NULL, "
-                + " CANTIDAD	REAL    NOT NULL " + ")";
+                + " ID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL ," +
+                "ID_USUARIO INTEGER NOT NULL, "
+                + " ID_MONEDA INTEGER NOT NULL, "
+                + " CANTIDAD REAL NOT NULL, "
+                + " FOREIGN KEY(ID_USUARIO) REFERENCES USUARIO(ID),"
+                + " FOREIGN KEY(ID_MONEDA) REFERENCES MONEDA(ID) "
+                + ")";
         stmt.executeUpdate(sql);
         sql = "CREATE TABLE IF NOT EXISTS ACTIVO_FIAT"
                 + "("
-                + " NOMENCLATURA VARCHAR(10)  PRIMARY KEY     NOT NULL, "
-                + " CANTIDAD	REAL    NOT NULL " + ")";
+                + " ID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL ,"+  "ID_USUARIO INTEGER NOT NULL, "
+                + " ID_MONEDA INTEGER NOT NULL, "
+                + " CANTIDAD REAL NOT NULL, "
+                + " FOREIGN KEY(ID_USUARIO) REFERENCES USUARIO(ID),"
+                + " FOREIGN KEY(ID_MONEDA) REFERENCES MONEDA(ID)"
+                + ")";
         stmt.executeUpdate(sql);
         sql = "CREATE TABLE IF NOT EXISTS TRANSACCION"
                 + "("
-                + " RESUMEN VARCHAR(1000)   NOT NULL, "
-                + " FECHA_HORA		DATETIME  NOT NULL " + ")";
+                + " ID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL , "
+                + " RESUMEN VARCHAR(1000) NOT NULL, "
+                + " FECHA_HORA DATETIME NOT NULL "
+                + " ID_USUARIO INTEGER NOT NULL, "
+                + " FOREIGN KEY(ID_USUARIO) REFERENCES USUARIO(ID)"
+                + ")";
         stmt.executeUpdate(sql);
         stmt.close();
     }
---
+++ OctoWallet/src/Octo/Servicios/CotizacionesRequest.java
@@ -0,0 +1,6 @@
+package Octo.Servicios;
+
+public class CotizacionesRequest {
+    // no me gusta lo proporcionado, quiero hacerlo asincronico con Future
+    // cambiar parseo con jackson en vez de org json (más simple y escalable)
+}
